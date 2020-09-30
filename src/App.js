02d2850a0b780c36fcc8d749e3860420a69b301e
haris-aqeel <harisaqeel.2001@gmail.com>
2020-09-30 13:52:40 +0500

    Project 4-A Completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import useWebAnimations from "@wellyshen/use-web-animations";
+import Wheel1 from './Wheel1'
+import Wheel2 from './Wheel2'
+
 import './App.css';
 
 function App() {
+
+  const [speed, setspeed] = useState(1); 
+
+  const {ref, playState, getAnimation} = useWebAnimations({
+    keyframes: {
+      transform: ['translateX(-200px)']
+    },
+    timing: {
+      iterations: Infinity,
+      duration: 500,
+      delay: 1000,
+    }})
+
+    const play = () => {
+      getAnimation().play();
+    };
+  
+    const pause = () => {
+      getAnimation().pause();
+    };
+
+    const IncreaseSpeed = _ => {
+      let currRate = getAnimation().playbackRate;
+      setspeed(currRate*1.5)
+      getAnimation().updatePlaybackRate(speed)
+    }
+    
+    const DecreaseSpeed = _ => {
+      let currRate = getAnimation().playbackRate;
+      setspeed(currRate/1.5)
+      getAnimation().updatePlaybackRate(speed)
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="sky">
+      <button id='play' onClick={play}> Play </button>
+      <button id='pause' onClick={pause}> Pause</button>
+      <button id='pause' onClick={IncreaseSpeed}> Increase Speed</button>
+      <button id='pause' onClick={DecreaseSpeed}> Decrease Speed</button>
+      <div className='trees'>
+      <img src='http://hmcaranimation.surge.sh/Imagesandsound/trees.png' alt='trees'/>
+      </div>
+      <div className='road' ref={ref}>
+        <img src='http://hmcaranimation.surge.sh/Imagesandsound/track.png' alt='road'/>
+      </div>
+      <div className='car'>
+        <img
+        alt='road'
+        src='http://hmcaranimation.surge.sh/Imagesandsound/car_body.png'
+        width='740px'
+        height= '270px'
+        />
+      </div>
+      <div className='wheel' >
+        <Wheel1 state={playState} speed = {speed}/>
+        <Wheel2 state={playState} speed= {speed}/>
+      </div>
+        
     </div>
   );
 }
+
+
+
 
 export default App;
---
+++ src/Wheel2.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import useWebAnimations from "@wellyshen/use-web-animations";
+
+const Wheel2 = (props) => {
+    
+  
+    const {ref, getAnimation} = useWebAnimations({
+        keyframes: {
+          transform: ['rotate(360deg)']
+        },
+        timing: {
+          iterations: Infinity,
+          duration: 2000,
+          delay: 1000,
+        }})
+        const play = () => {
+            getAnimation().play();
+          };
+        
+          const pause = () => {
+            getAnimation().pause();
+          };
+    
+        if (props.state === 'paused'){
+            pause()
+        }else if (props.state === 'running'){
+            play()
+            getAnimation().updatePlaybackRate(props.speed)
+            
+        }
+
+        
+        
+        return (
+        <img id='wheel2' ref={ref} alt='wheel' src='http://hmcaranimation.surge.sh/Imagesandsound/car_wheel_left.png'/>
+    )
+}
+
+export default Wheel2
